65f8f1a44a3099379807d54b5519e31223023bc4
byungminchoi <bchoi@bizflow.com>
2020-12-22 10:15:49 +0900

    36. Adding Functionality With Javascript

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -1,1 +1,25 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMedioStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('Media stream error: ', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+
+    await videoElement.requestPictureInPicture();
+    
+    button.disabled = false;
+});
+
+//  On Load
+selectMedioStream();
